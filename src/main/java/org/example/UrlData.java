df5bf0ad1d57e17478eb2cbf5e98eed4af9ce16c
Pedro Henrique Jose Lopes <112137131+pedrohjlopes@users.noreply.github.com>
2024-11-20 21:26:06 -0300

    Criando lambda encurtador de url
    lambda

---
+++ src/main/java/org/example/UrlData.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UrlData {
+    private String originalUrl;
+    private long expirationTime;
+}
